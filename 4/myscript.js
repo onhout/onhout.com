753ff92108be005a3ab2cf79193c805d2547f6f7
Peter Liu <onhout@gmail.com>
2014-12-25 22:42:40 -0800

    Minor extension

--- 4/myscript.js
+++ 4/myscript.js
@@ -1,5 +1,20 @@
-var hello = function ($scope) {
-    $scope.getName = function(){
+var MAX_LEN = 200;
+var HelloController = function ($scope) {
+    $scope.name = "world"
+    $scope.getName = function () {
         return $scope.name;
+    }
+
+    $scope.remaining = function () {
+        return MAX_LEN - $scope.name.length;
+    }
+}
+
+var ButtonController = function($scope){
+    $scope.send = function(){
+        alert("Your name is: "+$scope.name);
+    }
+
+    $scope.clear = function(){
     }
 }
