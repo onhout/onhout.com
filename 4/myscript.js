62c454125fbe21030304abe54daa33a61c2fa076
Peter Liu <onhout@gmail.com>
2014-12-27 04:00:30 -0800

    Fixed it up with angular modules

--- 4/myscript.js
+++ 4/myscript.js
@@ -1,20 +1,24 @@
 var MAX_LEN = 200;
-var HelloController = function ($scope) {
-    $scope.name = "world"
+
+angular.module('hello', []).controller('HelloController', function($scope){
+    $scope.name = "world";
     $scope.getName = function () {
         return $scope.name;
-    }
-
+    };
     $scope.remaining = function () {
         return MAX_LEN - $scope.name.length;
-    }
-}
+    };
+});
+
+//var HelloController = function ($scope) {
+
+//};
 
 var ButtonController = function($scope){
     $scope.send = function(){
         alert("Your name is: "+$scope.name);
-    }
+    };
 
     $scope.clear = function(){
-    }
-}
+    };
+};
