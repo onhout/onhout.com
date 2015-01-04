ac4bbd0d2db4b0ba718638c01dfc535846034903
Peter Liu <onhout@gmail.com>
2015-01-04 00:12:29 -0800

    moved file

--- bigproject/pgs/homeController.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by pl on 1/2/2015.
- */
-
-angular.module('home', []).controller('homePageController', function($scope){
-    $scope.message = 'HOME PAGE WELCOMES U';
-});
-console.log("home controller loaded");
