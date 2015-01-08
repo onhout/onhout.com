f9d7d90ca0340a1f4f878d452942e7ca5c6ba1d7
Peter Liu <onhout@gmail.com>
2015-01-08 08:30:35 -0800

    added form validation on register

--- bigproject/controllers/user.js
+++ bigproject/controllers/user.js
@@ -2,7 +2,7 @@
  * Created by pl on 1/3/2015.
  */
 var user = angular.module('usersApp',[]);
-user.controller('userController', function($scope, $http){
+user.controller('logInController', function($scope, $http){
     var currentUrl = window.location.href;
     /*$scope.fields=[
         {placeholder: 'Username', name:'userName', isRequired:true},
@@ -33,22 +33,24 @@ user.controller('userController', function($scope, $http){
             $scope.errorMessage = "Please check your logins and try again";
         })
     };
-
+});
+user.controller('registrationController', function($scope, $http){
+    var currentUrl = window.location.href;
     $scope.register = function(){
-      var request = $http({
-          method: "post",
-          url: currentUrl.substring(0, currentUrl.lastIndexOf("index.html")) + "data/register.php",
-          data: {
-              userName: $scope.userName,
-              password: $scope.password,
-              email: $scope.email,
-              first_name: $scope.first_name,
-              last_name: $scope.last_name,
-              business_name: $scope.business_name,
-              business_address: $scope.business_address,
-              business_phone: $scope.business_phone
-          }
-      });
+        var request = $http({
+            method: "post",
+            url: currentUrl.substring(0, currentUrl.lastIndexOf("index.html")) + "data/register.php",
+            data: {
+                userName: $scope.userName,
+                password: $scope.password,
+                email: $scope.email,
+                first_name: $scope.first_name,
+                last_name: $scope.last_name,
+                business_name: $scope.business_name,
+                business_address: $scope.business_address,
+                business_phone: $scope.business_phone
+            }
+        });
         request.success(function (data){
             $scope.ok(data);
             console.log("registration success");
@@ -58,3 +60,17 @@ user.controller('userController', function($scope, $http){
         });
     };
 });
+
+user.directive('pwConfirm', [function(){
+    return {
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ctrl){
+            var password = attrs.ngModel;
+            var confirmpassword = attrs.pwConfirm;
+            scope.$watch('[password, confirmpassword]', function(value){
+                ctrl.$setValidity('pwMatch', scope[password]===scope[confirmpassword]);
+            });
+        }
+    }
+}]);
+
