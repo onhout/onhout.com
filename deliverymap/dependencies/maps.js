2670dc342283fa6e973e7e6eb12de3a2bdcc5f0a
Peter Liu <onhout@gmail.com>
2015-02-02 22:43:56 -0800

    coords completely iterated

--- deliverymap/dependencies/maps.js
+++ deliverymap/dependencies/maps.js
@@ -1,7 +1,7 @@
 /**
  * Created by pl on 1/17/2015.
  */
-$(document).ready(function() {
+$(document).ready(function () {
     var mapOptions = {
         zoom: 14,
         center: new google.maps.LatLng(37.765, -122.263)
@@ -10,52 +10,51 @@ $(document).ready(function() {
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
 
-    var bounds =[];
-
-    var drawingmanager = new google.maps.drawing.DrawingManager({
-        drawingMode : google.maps.drawing.OverlayType.POLYGON,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_CENTER
-        }
-    });
-
-    google.maps.event.addDomListener(drawingmanager, 'polygoncomplete', function(thepolygon){
-        thepolygon.setEditable(true);
-        bounds.push(thepolygon);
-    });
-
-    var polygon = new google.maps.Polygon({
-        paths: bounds,
+    var bounds = [];
+    var polygonoptions = {
         editable: true,
-        strokeColor: '#FF0000',
+        strokeWeight:0.8,
+        strokeColor: "#0000FF",
         strokeOpacity: 0.9,
         fillColor: '#FF0000',
         fillOpacity: 0.35
+    };
+
+    var drawingmanager = new google.maps.drawing.DrawingManager({
+        drawingControl: true,
+        drawingControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+        },
+        polygonOptions: polygonoptions,
+        map:map
     });
 
-    console.log(polygon);
+
+
+    google.maps.event.addDomListener(drawingmanager, 'polygoncomplete', function (thepolygon) {
+        bounds.push(thepolygon);
+    });
 
     var coordbutton = document.getElementById('showstrings');
-    google.maps.event.addDomListener(coordbutton, 'click', function(){
-        for (var i = 0; i < bounds.length; i++){
+    google.maps.event.addDomListener(coordbutton, 'click', function () {
+        $("#contentString").html('');
+        for (var i = 0; i < bounds.length; i++) {
             var polybounds = bounds[i].getPath();
+            var contentString = '';
             console.log(polybounds);
-            var xy;
-            polybounds.forEach(function(xy, i) {
-                var contentString = '<br>' + 'Coordinate: ' + i + '<br>' + xy.lat() +',' + xy.lng();
+            polybounds.forEach(function (xy, i) {
+                var contentString = '<br>' + 'Coordinate: ' + i + '<br>' + xy.lat() + ',' + xy.lng();
                 $("#contentString").append(contentString);
             });
-
-    }
+        }
     });
-       /* new google.maps.LatLng(37.6, -122.27),
-        new google.maps.LatLng(37.8, -122.25),
-        new google.maps.LatLng(37.7, -122.26)
-    );*/
+    /* new google.maps.LatLng(37.6, -122.27),
+     new google.maps.LatLng(37.8, -122.25),
+     new google.maps.LatLng(37.7, -122.26)
+     );*/
 
-    polygon.setMap(map);
-    drawingmanager.setMap(map);
+    //drawingmanager.setMap(map);
 
 });
 
