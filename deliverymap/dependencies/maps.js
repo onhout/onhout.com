d51904c37d0096e8971837941584acb350b8de78
Peter Liu <onhout@gmail.com>
2015-02-04 09:17:45 -0800

    deleted unnecessary comments

--- deliverymap/dependencies/maps.js
+++ deliverymap/dependencies/maps.js
@@ -44,15 +44,12 @@ $(document).ready(function () {
     //VERY GENERIC FUNCTION HAHAHA
     $(".dacheck").change(function () {
         var namespace = $(this).attr('id');
-        console.log(datas[namespace]);
         if ($(this).is(":checked")) {
             if (datas[namespace].length == 0/*check if array is empty*/) {
                 $.get("dependencies/mapjson/"+namespace+".json", function (data) {
                     for (var i = 0; i < data.length; i++) {
                         datas[namespace].push(new google.maps.LatLng(data[i][0], data[i][1]));
-                        console.log("x: " + data[i][0] + " y: " + data[i][1]);
                     }
-                    console.log(datas[namespace]);
                 }).done(function () {
                     shapeLibrary[namespace].setPath(datas[namespace]);
                 });
@@ -77,24 +74,6 @@ $(document).ready(function () {
         });
     });
 
-    /*google.maps.event.addDomListener(document.getElementById('loaddata'), 'click', function () {
-        lakeshoreShape.setMap(map);
-    });*/
-
-    /*var drawingmanager = new google.maps.drawing.DrawingManager({
-     drawingControl: true,
-     drawingControlOptions: {
-     position: google.maps.ControlPosition.TOP_CENTER,
-     drawingModes: [google.maps.drawing.OverlayType.POLYGON]
-     },
-     polygonOptions: polygonoptions,
-     map:map
-     });
-
-     google.maps.event.addDomListener(drawingmanager, 'polygoncomplete', function (thepolygon) {
-     bounds.push(thepolygon);
-     });*/
-
     var coordbutton = document.getElementById('savedata');
     google.maps.event.addDomListener(coordbutton, 'click', function () {
         $("#contentString").html('');
@@ -102,7 +81,6 @@ $(document).ready(function () {
             var namespace = $(this).attr("id");
             datas[namespace] = [];
             var polybounds=shapeLibrary[namespace].getPath().getArray();
-            console.log(shapeLibrary[namespace]);
             polybounds.forEach(function (xy, i) {
                 var contentString = '<br>' + '<b>' +namespace+ ' </b>'+'Coordinate: ' + i + '<br>' + xy.lat() + ',' + xy.lng();
                 $("#contentString").append(contentString);
@@ -110,7 +88,6 @@ $(document).ready(function () {
                 temparray.push(xy.lat(), xy.lng());
                 datas[namespace].push(temparray);
             });
-            console.log(namespace);
             $.ajax({
                 type: 'POST',
                 url: 'dependencies/savecoords.php',
@@ -122,36 +99,5 @@ $(document).ready(function () {
         });
 
     });
-
-    /*var showdata = document.getElementById('loaddata');
-    google.maps.event.addDomListener(showdata, 'click', function () {
-        $.get("dependencies/mapjson/lakeshore.json", function (data) {
-            for (var i = 0; i < data.length; i++) {
-                lakeshorebounds.push(new google.maps.LatLng(data[i][0], data[i][1]));
-                console.log("x: " + data[i][0] + " y: " + data[i][1]);
-            }
-            console.log(jsoncoords);
-            var bermudaTriangle = new google.maps.Polygon({
-                paths: jsoncoords,
-                editable: true,
-                strokeColor: '#FF0000',
-                strokeOpacity: 0.8,
-                strokeWeight: 1,
-                fillColor: '#FF0000',
-                fillOpacity: 0.35
-            });
-            bermudaTriangle.setMap(map);
-            console.log(bermudaTriangle);
-        });
-    });*/
-
-
-    /* new google.maps.LatLng(37.6, -122.27),
-     new google.maps.LatLng(37.8, -122.25),
-     new google.maps.LatLng(37.7, -122.26)
-     );*/
-
-    //drawingmanager.setMap(map);
-
 });
 
